caed4bef87c6c3b98a0c93854397375a6e4ccce2
Lorens123 <lorens.vinge@gmail.com>
2021-10-14 11:07:44 +0200

    Skapat en Json file jippi

--- SavingGrejs/Program.cs
+++ SavingGrejs/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using System.IO;
+using System.Runtime.ConstrainedExecution;
+using System;
+using System.Text.Json;
 
 namespace SavingGrejs
 {
@@ -7,9 +10,22 @@ namespace SavingGrejs
         static void Main(string[] args)
         {
 
-            Karaktär knight = new Karaktär();
+            Karaktär karaktär = new Karaktär();
 
 
+            Console.WriteLine("Ge din karaktär ett namn: ");
+            karaktär.name = Console.ReadLine();
+            Console.WriteLine("Hur mycket damage ger " + karaktär.name + "? ");
+            karaktär.damage = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur mycket hp har din " + karaktär.name + "? ");
+            karaktär.hitPoints = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"Namn: {karaktär.name} | Damage: {karaktär.damage} | HP: {karaktär.hitPoints} ");
+            string karaktjson = JsonSerializer.Serialize<Karaktär>(karaktär);
+
+            Console.WriteLine(karaktjson);
+            File.WriteAllText("karaktar.json", karaktjson);
 
 
 
